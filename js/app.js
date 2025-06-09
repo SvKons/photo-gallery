83ce24c7651b76b1e35d4a3ae56335e6b2026782
Sveta Konstantinova <svetikons@mail.ru>
2025-06-09 22:57:17 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true,
+});
+
+let itemsL = gsap.utils.toArray('.gallery__left .gallery__item');
+
+itemsL.forEach(item => {
+    gsap.fromTo(
+        item,
+        { opacity: 0, x: -90 },
+        {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: 'top 130%',
+                end: 'top 50%',
+                scrub: true,
+            },
+        }
+    );
+});
+
+let itemsR = gsap.utils.toArray('.gallery__right .gallery__item');
+
+itemsR.forEach(item => {
+    gsap.fromTo(
+        item,
+        { opacity: 0, x: 100 },
+        {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: 'top 150%',
+                end: 'top 50%',
+                scrub: true,
+            },
+        }
+    );
+});
